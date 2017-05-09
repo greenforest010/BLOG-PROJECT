7fb05216055ca6c097ad99eafcff979fc2a993b2
green010 <foopop@hanmail.net>
2017-05-09 20:06:50 +0900

    Blog Post 읽기
    Post 제목 클릭 시 detail 화면으로 이동

--- src/main/java/com/growingitskill/controller/IndexController.java
+++ src/main/java/com/growingitskill/controller/IndexController.java
@@ -3,8 +3,10 @@ package com.growingitskill.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.growingitskill.service.PostService;
 
@@ -27,8 +29,10 @@ public class IndexController {
 		return "about";
 	}
 	
-	@RequestMapping(value = "details", method = RequestMethod.GET)
-	public String details() {
+	@RequestMapping(value = "/post/{id}", method=RequestMethod.GET)
+	public String read(@PathVariable("id") long id, Model model) throws Exception {
+		model.addAttribute(postService.read(id));
+		
 		return "details";
 	}
 
