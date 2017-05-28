46cf933ecba842c583f90258fa4f1196ca642d47
green010 <foopop@hanmail.net>
2017-05-29 03:11:30 +0900

    카테고리가 삭제된 글은 글 리스트 페이지에서 새로 카테고리 설정 가능

--- src/main/java/com/growingitskill/controller/PostController.java
+++ src/main/java/com/growingitskill/controller/PostController.java
@@ -1,20 +1,24 @@
 package com.growingitskill.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.growingitskill.domain.CategoryVO;
 import com.growingitskill.domain.MemberVO;
 import com.growingitskill.domain.PostVO;
+import com.growingitskill.mapper.CategoryRelationMapper;
 import com.growingitskill.service.CategoryService;
 import com.growingitskill.service.MemberService;
 import com.growingitskill.service.PostService;
@@ -33,6 +37,9 @@ public class PostController {
 
 	@Autowired
 	private CategoryService categoryService;
+	
+	@Autowired
+	private CategoryRelationMapper categoryRelationMapper;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String listPage(Model model) throws Exception {	
@@ -43,8 +50,26 @@ public class PostController {
 		}
 		
 		model.addAttribute("list", postService.listAll());
+		model.addAttribute("categoryList", categoryService.listAll());
 
 		return "admin/post/list";
+	}
+	
+	@ResponseBody
+	@RequestMapping(method = RequestMethod.POST)
+	public PostVO listPage(@RequestBody Map<String, Long> map) throws Exception {
+		CategoryVO categoryVO = new CategoryVO();
+		categoryVO.setId(map.get("categoryId"));
+		
+		PostVO postVO = new PostVO();
+		postVO.setId(map.get("id"));
+		postVO.setCategoryVO(categoryVO);
+		
+		categoryRelationMapper.create(postVO);
+		
+		PostVO result = postService.read(map.get("id"));
+		
+		return result;
 	}
 
 	@RequestMapping(value = "new", method = RequestMethod.GET)
